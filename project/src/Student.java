7a2a4bf299903474ec261daa8550d5cf0e10bd34
Danailnd <danaildimitrov01@gmail.com>
2025-05-18 16:26:00 +0300

    Add student object

---
+++ project/src/Student.java
@@ -0,0 +1,78 @@
+public class Student {
+
+    private String name;
+    private String facultyNumber;
+    private int course;
+    private String specialty;
+    private int group;
+    private StudentStatus status;
+    private double averageGrade;
+
+    public Student(String name, String facultyNumber, int course, String specialty, int group, StudentStatus status, double averageGrade) {
+        this.name = name;
+        this.facultyNumber = facultyNumber;
+        this.course = course;
+        this.specialty = specialty;
+        this.group = group;
+        this.status = status;
+        this.averageGrade = averageGrade;
+    }
+
+
+//    getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public String getFacultyNumber() {
+        return facultyNumber;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public StudentStatus getStatus() {
+        return status;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFacultyNumber(String facultyNumber) {
+        this.facultyNumber = facultyNumber;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public void setStatus(StudentStatus status) {
+        this.status = status;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+}
